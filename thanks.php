6dda9727e6ed7088457572eabff6da6eaa38cdb3
Stéphane Englebert <stephane.englebert@gmail.com>
2021-03-24 10:14:15 +0100

    first commit thanks.php

---
+++ thanks.php
@@ -0,0 +1,40 @@
+<?php include "validate.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hackers-Poulette Contact support</title>
+    <style>
+        .err_msg{color:red;}
+    </style>
+</head>
+<body>
+    <header>
+        <a href="#"><img id="logo" src="assets/img/logo.png" width="100" height="100" alt="Logo hackers-poulette"></a>
+        <nav>
+            <ol>
+                <li><a href="#">Home</a></li>
+                <li><a href="#">xxx</a></li>
+                <li><a href="#">Support</a></li>
+            </ol>
+        </nav>
+    </header>
+    <main>
+        <section>
+            <img src="" alt="raspberry pi illustration">
+            <h2>Thanks!</h2>
+            <p>We received your message and registered it under reference 123456789</p>
+            <p>Please mention this reference in any subsequent mail exchange.</p>
+            <p>Thanks for your feedback!.  Our support team will follow up and get back to you within 48 hours.</p>
+            <p>A copy of this message was sent on your email address.</p>
+        </section>
+    </main>
+    <footer>
+        <section>
+            © 2021 Hackers-Poulette
+        </section>
+    </footer>
+</body>
+</html>
